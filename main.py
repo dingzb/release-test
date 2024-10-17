c5fea837136c8f5a3a7211761cd8b329c1f46018
Dingzb <dingzibiao@gmail.com>
2024-10-17 18:25:17 +0800

    feature: Add print_hello_name function.

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def print_hello():
     print('Hello World!')
 
 
+def print_hello_name(name):
+    print(f'Hello {name}')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
